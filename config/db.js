48c4416a0fd265f6a9572b2d80788fde731f4d47
Martin <martindesch14@gmail.com>
2024-12-16 19:10:44 -0300

    consolelog

--- config/db.js
+++ config/db.js
@@ -5,12 +5,17 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        console.log('MONGO_URI:', process.env.MONGO_URI); // Esto imprime el valor de MONGO_URI
+        if (!process.env.MONGO_URI) {
+            throw new Error('La variable MONGO_URI no está definida');
+        }
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log('MongoDB conectado correctamente');
     } catch (error) {
         console.error('Error conectando a MongoDB:', error.message);
         process.exit(1);
     }
 };
-
-module.exports = connectDB;
